51c7d27ec93aecd72c0c39419ef744af0e6aa45e
pavanratnakar07 <pavanratnakar124@gmail.com>
2024-08-27 15:35:16 +0530

    created

---
+++ src/main/java/com/TechEazy_Consulting/LMDS/LmdsApplication.java
@@ -0,0 +1,13 @@
+package com.TechEazy_Consulting.LMDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TechEazy_Consulting/LMDS/LmdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechEazy_Consulting.LMDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
