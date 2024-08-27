49816394396d347b2b48df0d6d62eb622a3bb334
Chandan Pandey <cpandey05@gmail.com>
2024-08-27 17:03:09 +0530

    renamed project and package name

--- src/main/java/com/techeazy/lmds/LmdsApplication.java
+++ src/main/java/com/techeazy/lmds/LmdsApplication.java
@@ -1,4 +1,4 @@
-package com.TechEazy_Consulting.LMDS;
+package com.techeazy.lmds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
