8dddf4adbf193057751f5c89ba074c283c8ecdad
Revellipavan <pavanrevelli9@gmail.com>
2024-08-28 23:07:16 +0530

    initial commit

---
+++ src/main/java/com/techeazy/lmds/HelloController.java
@@ -0,0 +1,14 @@
+package com.techeazy.lmds;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+   
+    public String hello(){
+    return "Hello World";
+    }
+}
