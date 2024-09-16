e2a5a4e18c7bbf620ce9690d90b36736613863e7
Sapkal-Suraj <168258961+Sapkal-Suraj@users.noreply.github.com>
2024-09-16 21:14:12 +0530

    Add files via upload

---
+++ TechEazyApplication.java
@@ -0,0 +1,15 @@
+package com.techEazy.techEazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEazyApplication.class, args);
+		System.out.println("start project");
+		
+	}
+
+}
